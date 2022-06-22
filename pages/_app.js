46bf15945b8c7b4c33069f1fdf8f2cf65e19b8b7
Barry Wire <bmwemawire@gmail.com>
2022-06-22 14:38:23 +0300

    chore: remove bootstrap, bootstrap-icons; install tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,8 @@
-// Bootstrap imports
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
 // Custom styles import
 import '../styles/global.css';
 
-// React imports
-import { useEffect } from 'react';
-
 function MyApp({ Component, pageProps })
 {
-  useEffect(() =>
-  {
-    import('bootstrap/dist/js/bootstrap');
-  }, []);
-
   return <Component {...pageProps} />
 }
 
